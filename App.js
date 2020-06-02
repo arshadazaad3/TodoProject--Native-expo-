2288a8f047b6262b13a6185afee5570ec24ce276
ArshadAzaad3299 <arshad.2017094@iit.ac.lk>
2020-06-02 18:13:25 +0530

    v1

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import React from 'react'
+import {decode, encode} from 'base-64'
+import { YellowBox } from 'react-native';
+import _ from 'lodash';
+
+import MainWindow from './components/MainWindow'
+
+if (!global.btoa) {  global.btoa = encode }
+
+if (!global.atob) { global.atob = decode }
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
+
+
+export function registerScreens() {
+  Navigation.registerComponent('example.MainWindow', () => gestureHandlerRootHOC(MainWindow));
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App(){
+  return(
+    <MainWindow></MainWindow>
+
+  )
+}
+
